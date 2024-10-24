c3f7e09740c76adc2dc6937a27f989b41c5996bc
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2024-10-24 18:52:36 +0300

    Add dbErrorHandlers

--- middleware/errorHandlers.js
+++ middleware/errorHandlers.js
@@ -1,10 +1,34 @@
+const { ValidationError, BaseError } = require('sequelize');
+
+module.exports.dbErrorHandler = (err, req, res, next) => {
+  if (err instanceof ValidationError) {
+    const errors = err.errors.map(e => ({ status: 422, detail: e.message }));
+
+    return res.status(422).send(errors);
+  }
+  if (err instanceof BaseError) {
+    return res.status(500).send([
+      {
+        status: 500,
+        detail: 'DataBase Error',
+      },
+    ]);
+  }
+  next(err);
+};
+
 module.exports.errorHandler = (err, req, res, next) => {
   if (res.headersSent) {
     return;
   }
-
   const status = err.status || 500;
   const message = err.message || 'Server Error';
-
-  res.status(status).send(message);
+  res.status(status).send({
+    errors: [
+      {
+        status,
+        detail: message,
+      },
+    ],
+  });
 };
